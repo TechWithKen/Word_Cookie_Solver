c8c9bed49ab9a20b40f55856fb9fccbf9f550764
TechWithKen <ti664469@gmail.com>
2024-05-30 17:58:41 +0100

    reverted back to a default mode as local server code isn't accesible

--- Cookie_Solver/wordcookie/settings.py
+++ Cookie_Solver/wordcookie/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7t!kw_6%t8$+h=k4mo86uhw)9lgvac0v9+$@llw9ma!)0dc!&@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
